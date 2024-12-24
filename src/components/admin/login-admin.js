817510e06dc8276bb3d25bf9eb8aeb708d1fcfef
EEslamo <141227368+EEslamo@users.noreply.github.com>
2024-12-24 14:45:13 +0200

    Update login-admin.js

--- src/components/admin/login-admin.js
+++ src/components/admin/login-admin.js
@@ -10,7 +10,7 @@ function LoginAdmin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/login-admin', {
+      const response = await fetch('https://tools-3-backend-git-youssefhatem03-dev.apps.rm2.thpm.p1.openshiftapps.com/login-admin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
